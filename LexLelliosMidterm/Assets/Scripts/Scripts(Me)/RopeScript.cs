6905c0ad80e579e491e1e637772d598d02f97be6
amlellios <alexis.lellios@loop.colum.edu>
2018-03-26 01:53:19 -0500

    Midterm

---
+++ LexLelliosMidterm/Assets/Scripts/Scripts(Me)/RopeScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<CharacterJoint>().connectedBody.transform.parent.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ LexLelliosMidterm/Assets/Scripts/Scripts(Me)/Rotator.cs
@@ -0,0 +1,18 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    private int spinx = 0;
+    private int spiny = 7;
+    private int spinz = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(spinx, spiny, spinz);
+    }
+}
