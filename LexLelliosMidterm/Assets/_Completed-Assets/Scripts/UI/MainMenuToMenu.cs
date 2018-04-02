8e0ada4d8f4f2479ce933b25091931f246e47438
amlellios <alexis.lellios@loop.colum.edu>
2018-04-02 12:31:20 -0500

    final update

--- LexLelliosMidterm/Assets/_Completed-Assets/Scripts/UI/MainMenuToMenu.cs
+++ LexLelliosMidterm/Assets/_Completed-Assets/Scripts/UI/MainMenuToMenu.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class MainMenuToMenu : MonoBehaviour
 {
 
-    public void ButtonClicked()
+    public void StartButtonClicked()
     {
         // Load the next scene.
         SceneManager.LoadScene("MenuStart");
---
+++ LexLelliosMidterm/Assets/_Completed-Assets/Scripts/UI/Newmusic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Newmusic : MonoBehaviour
+
+//{
+
+
+//    void DestroyGameObject()
+//    {
+
+
+//        DestroyGameObject(GameMusic);
+
+
+//    }
+       
+
+
+
+
+        
+
+
+//}
